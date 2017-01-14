298eeaf059f40d5c6671184950f714ca84fc910e
Francisco Maseda Muiño <frmadem@gmail.com>
2017-01-14 20:06:28 +0100

    Added on_up event

--- lib/PrefApp/Puzzle/Process.pm
+++ lib/PrefApp/Puzzle/Process.pm
@@ -128,6 +128,18 @@ sub up{
 
             $self->dockerCommands->upService($service);
 
+            unless($f_in_recompilation){
+
+                $self->eventCommands->fireEventForService(
+
+                    "on_up",
+
+                    $service
+
+                );
+
+            }
+
         } 
     }
 
